8a2a0d1c01cfd2d99f50753b49aa271b4bc2d95c
lucascavalcante <lucascavalcante@gmail.com>
2021-04-08 00:31:57 -0300

    Create .env file

--- db.sh
+++ db.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cp src/.env.example src/.env
 docker exec -it casafy-app php artisan migrate:install
 docker exec -it casafy-app php artisan migrate
 docker exec -it casafy-app php artisan db:seed
